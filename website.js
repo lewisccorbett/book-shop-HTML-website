47b85080f03f389543920f642830832b7c995039
lewisccorbett <lewisccorbett@gmail.com>
2025-03-05 18:55:47 +0000

    Create website.js

---
+++ website.js
@@ -0,0 +1,4 @@
+function display(book) {
+    book1= document.getElementById(book)
+    book1.style.visibility = (book1.style.visibility == "visible") ? "hidden" : "visible";  // this checks if the visibility is visible or hidden if its visible make it hidden and vice versa
+}   
